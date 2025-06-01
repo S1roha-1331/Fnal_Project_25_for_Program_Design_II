fc2bcca8f49cd60d422976392c2e25ecea1146ac
steven-ycy <ycysteven1214@gmail.com>
2025-06-01 22:26:22 +0800

    i dont know

---
+++ Finalproject/Assets/Scenes/loadcur.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+public class loadcur : MonoBehaviour
+{
+    public SceneSwitcher a;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+        // 判斷是否是特定標籤的物件
+        if (other.CompareTag("Player"))
+        {
+
+
+
+            a.Falling();
+
+            //StartCoroutine(FadeIn());
+          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+    }
+
+
+
+    
+    //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    
+
+
+}
+
--- Finalproject/Assets/Scenes/onclick.cs
+++ Finalproject/Assets/Scenes/onclick.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using System.Collections;
 public class SceneSwitcher : MonoBehaviour
 {
-   public void LoadNextScene()
+
+    public Image darkPanel;
+
+    public float duration = 1f;
+    public void LoadNextScene()
     {
       
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -17,6 +21,23 @@ public class SceneSwitcher : MonoBehaviour
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+    public void Falling()
+    {
+        StartCoroutine(FadeIn());
+    }
+    IEnumerator FadeIn()
+    {
+        float elapsed = 0f;
+        Color c = darkPanel.color;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            c.a = Mathf.Lerp(0, 0.8f, elapsed / duration);
+            darkPanel.color = c;
+            Debug.Log("Panel Alpha set to: " + darkPanel.color.a);
+            yield return null;
         }
     }
 }
