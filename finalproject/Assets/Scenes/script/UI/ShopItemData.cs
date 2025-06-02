d3623e417c4f56154332fc82708b74940d52137b
liupiyan <z0972757975@gmail.com>
2025-06-03 02:42:07 +0800

    finish shop

--- finalproject/Assets/Scenes/script/UI/ShopItemData.cs
+++ finalproject/Assets/Scenes/script/UI/ShopItemData.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewShopItem", menuName = "Shop/Item")]
+public enum StatType
+{
+    Attack,
+    Speed,
+    Health
+}
+
+[CreateAssetMenu(menuName = "Shop/ShopItemData")]
 public class ShopItemData : ScriptableObject
 {
     public string itemName;
-    public int price;
     public Sprite icon;
+    public int price;
+
+    public StatType targetStat;  // 升級目標
+    public float amount;         // 增加數值
 }
